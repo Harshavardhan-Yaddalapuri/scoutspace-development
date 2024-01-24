74f3872ffc5dc8d08a4a8a75b02872f6f09a1604
harshavardhan <haarshavardhan08@gmail.com>
2024-01-24 13:07:19 -0600

    Connecting to MongoDB

---
+++ api/src/main/java/com/noobprogrammer/scoutspace/ScoutSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.noobprogrammer.scoutspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutSpaceApplication.class, args);
+	}
+
+}
